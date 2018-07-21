3ecc02e12c6bbcce4598cc0ee8dbbd3d9b460cae
Zach Smith <zsmith126028@gmail.com>
2018-07-21 17:11:40 -0500

    Adding recent code, having trouble with MongoDb database. DB functions not working for some reason

--- app.js
+++ app.js
@@ -13,29 +13,33 @@ app.use(bodyParser.urlencoded({extended: true}));
 var statSchema = new mongoose.Schema({
     name: String,
     image: String,
-    stats: {
-        hitters: {
-            HitsAB: String,
-            R: String,
-            HR: String,
-            RBI: String,
-            SB: String,
-            OPS: String
-        },
-        pitchers: {
-            IP: String,
-            SV: String,
-            K: String,
-            ERA: String,
-            WHIP: String,
-            QS: String
-        }
-    }
+    HitsAB: String,
+    R: String,
+    HR: String,
+    RBI: String,
+    SB: String,
+    OPS: String
 });
 
-var Stats = mongoose.model("Stats", statSchema);
+var Stat = mongoose.model("Stat", statSchema);
 
-
+Stat.create({
+    name: "Kris Bryant",
+    image: "http://mlb.mlb.com/mlb/images/players/head_shot/592178.jpg",
+    HitsAB: "82/290",
+    R: "46",
+    HR: "11",
+    RBI: "42",
+    SB: "2",
+    OPS: ".878"
+}, function(err, stat) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("New Stats!");
+        console.log(stat);
+    }
+});
 
 
 
