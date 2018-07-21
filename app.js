82f80418c28a2c8cfe37151a3a7e2108cc4d1b58
Zach Smith <zsmith126028@gmail.com>
2018-07-21 15:32:01 -0500

    Added app configuration to app.js

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ app = express(),
 bodyParser = require("body-parser"),
 mongoose = require("mongoose");
 
+// App config
+mongoose.connect("mongodb://localhost/fantasy_stats");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
