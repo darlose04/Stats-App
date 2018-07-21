9d7029b90ac99d8647d7e8eb75cadb182db3096a
Zach Smith <zsmith126028@gmail.com>
2018-07-21 15:52:29 -0500

    Created MongoDB Schema and mongoose model as well started server

--- app.js
+++ app.js
@@ -8,3 +8,37 @@ mongoose.connect("mongodb://localhost/fantasy_stats");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Mongoose/model config
+var statSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    stats: {
+        hitters: {
+            HitsAB: String,
+            R: String,
+            HR: String,
+            RBI: String,
+            SB: String,
+            OPS: String
+        },
+        pitchers: {
+            IP: String,
+            SV: String,
+            K: String,
+            ERA: String,
+            WHIP: String,
+            QS: String
+        }
+    }
+});
+
+var Stats = mongoose.model("Stats", statSchema);
+
+
+
+
+
+app.listen(3000, function() {
+    console.log("Stats server has started!");
+});
